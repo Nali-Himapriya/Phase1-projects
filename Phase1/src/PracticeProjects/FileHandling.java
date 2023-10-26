873e25d7ef9b48620db775d1479486bd9d1e97b4
HP <HP@DESKTOP-RL4049L>
2023-10-26 18:55:51 +0530

    Initial Commit

---
+++ Phase1/src/PracticeProjects/FileHandling.java
@@ -0,0 +1,57 @@
+package PracticeProjects;
+
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+
+public class FileHandling {
+	
+
+	    public static void main(String[] args) {
+	        // Specify the file name
+	        String fileName = "sample.txt";
+
+	        // Step 1: Writing to a file
+	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+	            writer.write("Hi!");
+	            writer.newLine();
+	            writer.write("How are you ?");
+	        } catch (IOException e) {
+	            System.err.println("Error writing to the file: " + e.getMessage());
+	        }
+
+	        // Step 2: Reading from the file
+	        System.out.println("Reading file");
+	        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)) ) {
+	            String line;
+	            while ((line = reader.readLine()) != null) {
+	                System.out.println(line);
+	            }
+	        } catch (IOException e) {
+	            System.err.println("Error in reading the file: " + e.getMessage());
+	        }
+
+	        // Step 3: Appending to the file
+	        System.out.println("Appending to the file");
+	        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true)) ) {
+	            writer.write("\nWelcome to File Handling....");
+	            writer.newLine();
+	        } catch (IOException e) {
+	            System.err.println("Error in appending to the file: " + e.getMessage());
+	        }
+
+	        // Step 4: Reading from the file after appending
+	        System.out.println("Reading from the file after appending");
+	        try (BufferedReader reader = new BufferedReader(new FileReader(fileName)) ) {
+	            String line;
+	            while ((line = reader.readLine()) != null) {
+	                System.out.println(line);
+	            }
+	        } catch (IOException e) {
+	            System.err.println("Error reading from the file: " + e.getMessage());
+	        }
+	    }
+	}
+
